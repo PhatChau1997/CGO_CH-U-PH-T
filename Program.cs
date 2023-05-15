8ad56238a0527cfb922801247640fa1bab5f7ec9
PhatChau1997 <133665829+PhatChau1997@users.noreply.github.com>
2023-05-15 22:43:20 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CGO_CHÂU_PHÁT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
